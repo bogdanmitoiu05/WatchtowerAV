14b7f4cf581fc432363c22f58dd6984886f82fc2
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-17 20:59:08 +0300

    Initial scaffold

---
+++ src/main/java/ro/sda/watchtower/TrieByteMatcher.java
@@ -0,0 +1,11 @@
+package ro.sda.watchtower;
+
+/**
+ * Clasa pentru implementarea arborilor Tries
+ */
+public class TrieByteMatcher {
+
+
+
+
+}
---
+++ src/main/java/ro/sda/watchtower/TrieNode.java
@@ -0,0 +1,13 @@
+package ro.sda.watchtower;
+import java.util.List;
+import java.util.Optional;
+
+public class TrieNode {
+    private TrieNode[] children;
+    private Optional<Integer> matchId;
+
+
+    public TrieNode(){
+
+    }
+}
